b4698dc89a9e5ee62eda463e4960b5fc6471866a
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-03-13 02:31:47 -0300

    feat: primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        loadComponent: () => import('./Main/first-page/first-page.component')
+            .then(m => m.FirstPageComponent)
+
+    },
+    {
+        path: '**',
+        redirectTo: ''
+
+    }
+
+];
