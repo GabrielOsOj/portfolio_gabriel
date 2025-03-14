b9ecac60594dbf24c1ff9738591bb63350dd1b27
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-03-14 01:44:46 -0300

    feat: configurando variables y colores

--- src/app/Main/first-page/first-page.component.ts
+++ src/app/Main/first-page/first-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../Shared/navbar/navbar.component';
+import { FooterComponent } from "../../Shared/footer/footer.component";
 
 @Component({
   selector: 'app-first-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './first-page.component.html',
   styleUrl: './first-page.component.css'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstPageComponent } from "./Main/first-page/first-page.component";
+import { SecondPageComponent } from "./Main/second-page/second-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstPageComponent, SecondPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
