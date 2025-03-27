07537ed1d35da8ee6c5a1cbc32bd3102d269e775
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-03-27 11:33:13 -0300

    style: responsive pagina uno, navbar y footer

--- src/app/Shared/navbar/navbar.component.ts
+++ src/app/Shared/navbar/navbar.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { IconSvService } from '../../Core/services/icons/icon-sv.service';
+import { IconIF } from '../../Core/models/icon-if';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  menuIcon:string;
+  upArrowIcon:string;
+
+  menuOpen:boolean;
+
+  constructor(private iconSv:IconSvService){
+    this.menuIcon = iconSv.getUtilityIcon(<IconIF>{name:"menu"});
+    this.upArrowIcon = iconSv.getUtilityIcon(<IconIF>{name:"up_arrow"});
+    this.menuOpen = false;
+  }
+
+  public fnOpenMenu():void{
+    this.menuOpen = true;
+  }
+
+  public fnExitMenu():void{
+    this.menuOpen = false;
+  }
 
 }
